fabe38931a45459c8e72a3a6617497a059373557
douar <mouadouarzong@gmail.com>
2022-09-26 12:03:41 -0500

    added filter feature

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -10,32 +10,38 @@ export class ContactListComponent implements OnInit {
 
   @Input() list!: IContact[];
   @Output() newContact = new EventEmitter<undefined>();
-  @Output() onDelete = new EventEmitter<IContact>();
+  @Output() onDelete = new EventEmitter<string>();
   @Output() onUpdate = new EventEmitter<IContact>();
 
-  searchList!: IContact;
+  searchText: string = "";
+  displayList!: IContact[]
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
     console.log(this.list)
+    this.displayList = [...this.list];
   }
 
-  onClick(){
-    console.log('hello')
+  onNewContact(){
+    console.log('Console in Contact List Comp: New contact clicked!')
     this.newContact.emit();
   }
 
-  onContactDelete(contact: IContact){
-    this.onDelete.emit(contact);
+  onContactDelete(id: string){
+    this.onDelete.emit(id);
   }
 
   onContactUpdate(contact: IContact){
     this.onUpdate.emit(contact);
   }
 
-  onSearch(){
-    console.log('search clicked!')
+  filterList(searchText: string){
+    console.log(searchText);
+    this.displayList = this.list.filter((contact) => {
+      return contact.name.includes(searchText);
+    })
   }
 
 }
